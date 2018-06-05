5fa95f8491529fc1daa91918dff60cc448867260
Senk <paweldawidziak@gmail.com>
2018-06-05 11:23:29 +0200

    Notes are moving

---
+++ Assets/note.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class note : MonoBehaviour {
+
+    Rigidbody rb;
+    public float notesVelocity = 20;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        rb.velocity = new Vector3(0, 0, -notesVelocity);
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
