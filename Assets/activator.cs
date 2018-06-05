302cecfe4a519a8c782e364d8af030947d5e2de1
Senk <paweldawidziak@gmail.com>
2018-06-05 10:45:13 +0200

    Destroying notes while pressing activator key

--- Assets/activator.cs
+++ Assets/activator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class activator : MonoBehaviour {
 
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,24 @@ public class activator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(Input.GetKeyDown(key) && active)
+        {
+            Destroy(note);
+        }
+       
 	}
+    void OnTriggerEnter(Collider col)
+    {
+        active = true;
+        if(col.gameObject.tag=="Note")
+        {
+            note = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        active = false;
+    }
 }
