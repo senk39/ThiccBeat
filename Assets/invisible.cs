c34688fe0f74b6843f9341648ff672212b1126ce
Senk <paweldawidziak@gmail.com>
2018-06-06 12:08:21 +0200

    Invisible "good" buttons and score counter
    I just started to do score counter and it's on early stage of implementation.

---
+++ Assets/invisible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invisible : MonoBehaviour {
+    // MeshRenderer meshr;
+    //Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.GetComponent<Renderer>().enabled = false;
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
