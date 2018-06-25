e2afe49b5e21f00ea766d06b63ce42fafa8eb732
Senk <paweldawidziak@gmail.com>
2018-06-25 22:18:11 +0200

    Points are now correctly assgned to notes
    Now notes have their score. This score can be dynamically changed by position of the note.
    Anyway, at this moment, there's no reaction when player clicks keys.

--- Assets/Scripts/NoteBehaviour.cs
+++ Assets/Scripts/NoteBehaviour.cs
@@ -2,42 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteBehaviour : MonoBehaviour {
+public class NoteBehaviour : MonoBehaviour
+{
+    Transform tf;
 
     private uint missOrNotActivated = 0;
     private uint great = 50;
     private uint perfect = 200;
     public uint actualNoteValue;
 
+    public bool isNoteActive = false;
+
     public GameObject earlyActivator;
     public GameObject perfectActivator;
     public GameObject lateActivator;
 
+    private float row1 = -5.1f;
+    private float row2 = -3.1f;
+    private float row3 = -1.1f;
+    //====================
+    private float row4 = 1.1f;
+    private float row5 = 3.1f;
+    private float row6 = 5.1f;
 
+
+    void Awake()
+    {
+        tf = GetComponent<Transform>();
+    }
 
     // Use this for initialization
-    void Start () {
-
-        actualNoteValue = missOrNotActivated;
-
-}
-
-// Update is called once per frame
-void Update () {
-
-		
-	}
-
-    void OnCollisionStay(Collider col)
+    void Start()
     {
-        if (col.gameObject.tag == "TooEarlyIndicator" || col.gameObject.tag == "TooLateIndicator")
+        actualNoteValue = missOrNotActivated;
+        autoIndicatorSetter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actualNoteValue > 0)
         {
-            actualNoteValue = great;
+            isNoteActive = true;
         }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
         if (col.gameObject.tag == "Indicator")
         {
             actualNoteValue = perfect;
         }
+        if (col.gameObject.tag == "TooEarlyIndicator" || col.gameObject.tag == "TooLateIndicator")
+        {
+            actualNoteValue = great;
+        }
     }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Indicator")
+        {
+            actualNoteValue = great;
+        }
+        if (col.gameObject.tag == "TooLateIndicator")
+        {
+            actualNoteValue = missOrNotActivated;
+        }
+    }
+
+   void autoIndicatorSetter()
+    {
+        if (tf.position.x == row1)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 1");
+            perfectActivator = GameObject.Find("note indicator 1");
+            lateActivator = GameObject.Find("too late note indicator 1");
+        }
+
+        if (tf.position.x == row2)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 2");
+            perfectActivator = GameObject.Find("note indicator 2");
+            lateActivator = GameObject.Find("too late note indicator 2");
+        }
+
+        if (tf.position.x == row3)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 3");
+            perfectActivator = GameObject.Find("note indicator 3");
+            lateActivator = GameObject.Find("too late note indicator 3");
+        }
+
+        if (tf.position.x == row4)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 4");
+            perfectActivator = GameObject.Find("note indicator 4");
+            lateActivator = GameObject.Find("too late note indicator 4");
+        }
+
+        if (tf.position.x == row5)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 5");
+            perfectActivator = GameObject.Find("note indicator 5");
+            lateActivator = GameObject.Find("too late note indicator 5");
+        }
+
+        if (tf.position.x == row6)
+        {
+            earlyActivator = GameObject.Find("too early note indicator 6");
+            perfectActivator = GameObject.Find("note indicator 6");
+            lateActivator = GameObject.Find("too late note indicator 6");
+        }
+    }
+
 
 }
