3ae9711d56880b9e1bce5b08fc5e593d751e4818
Senk <paweldawidziak@gmail.com>
2018-07-05 21:48:33 +0200

    Revert "SCORE COUNTER DONE"
    This reverts commit 50d9172f3e665aed79e72e6785618939a357564b.

--- Assets/Scripts/activator.cs
+++ Assets/Scripts/activator.cs
@@ -6,18 +6,27 @@ using UnityEngine.UI;
 
 public class activator : MonoBehaviour {
 
+<<<<<<< HEAD
     //public int greatHitPoints = 50;
     //public int perfectHitPoints = 200;
+=======
+    public int greatHitPoints = 1;
+    public int perfectHitPoints = 1000;
+>>>>>>> parent of 50d9172... SCORE COUNTER DONE
 
     public KeyCode key;
     public bool active = false;
     public uint currentNoteValue = 0;
     public GameObject note;
 
+<<<<<<< HEAD
     public GameObject earlyTrigger;
     public GameObject lateTrigger;
 
 
+=======
+    //ActivatorType currentActivatorType = ActivatorType.Awaiting;
+>>>>>>> parent of 50d9172... SCORE COUNTER DONE
     public enum ActivatorType { Perfect, Great, Miss, Awaiting };
     public ActivatorType activatorType;
 
@@ -49,18 +58,30 @@ public class activator : MonoBehaviour {
             note.SetActive(false);
 
         }
+
     }
 
     void OnTriggerEnter(Collider col)
     {
-
-
-        if (col.gameObject.tag=="Note")
+        active = true;
+        if(col.gameObject.tag=="Note")
         {
-            active = true;
+            if (activatorType == ActivatorType.Perfect)
+            {
+                currentNoteValue = perfectHitPoints;
+            }
+
+            else
+            {
+                currentNoteValue = greatHitPoints;
+            }
+
             note = col.gameObject;
+<<<<<<< HEAD
             currentlyTriggeredNotes.Add(col.gameObject);
             destroyIfTooLate();       
+=======
+>>>>>>> parent of 50d9172... SCORE COUNTER DONE
         }
     }
 
@@ -80,6 +101,7 @@ public class activator : MonoBehaviour {
         playerScoreContainer.GetComponent<playerScore>().playerCurrentScore += actNoteValue;
     }
 
+<<<<<<< HEAD
     public void destroyIfTooLate()
     {
         
@@ -95,4 +117,7 @@ public class activator : MonoBehaviour {
 
    
 
+=======
+    
+>>>>>>> parent of 50d9172... SCORE COUNTER DONE
 }
--- Assets/Scripts/playerScore.cs
+++ Assets/Scripts/playerScore.cs
@@ -10,11 +10,19 @@ public class playerScore : MonoBehaviour
     public uint playerCurrentScore = 0;
     public Text playerScoreAsText;
 
+    
+
     // Update is called once per frame
     public void Update()
     {
+
         playerScoreAsText.text = playerCurrentScore.ToString();
-    }
+
+
+      
+        }
+
+   
 
 }
 
