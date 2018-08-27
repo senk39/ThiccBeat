7874edd10d5a68337610227ef6730f74b390914e
Senk <paweldawidziak@gmail.com>
2018-08-27 12:38:25 +0200

    Stripes are moving, but with different distances between them

---
+++ Assets/Scripts/TitleScreen/movingAllStripes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingAllStripes : MonoBehaviour {
+
+    public Vector3 pos;
+    public Transform tf;
+    public float speed;
+    
+
+// Use this for initialization
+void Start () {
+        pos = tf.position;
+        speed = 5f;
+        Debug.Log(pos);
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pos.y = 180f;
+        if (pos.x >= 755f)           //755 było prawie ok wartocią
+
+        {
+            pos.x = -455f;
+           
+
+        }
+        else
+        {
+            pos.x += speed;
+            tf.position = pos;
+        }
+    }
+}
