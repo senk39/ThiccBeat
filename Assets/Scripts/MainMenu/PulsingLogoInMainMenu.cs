faeb4c83c6cba38e69be8f5460e4ec89d5f49ca0
Senk <paweldawidziak@gmail.com>
2018-08-29 12:37:33 +0200

    Preparing Main Menu

---
+++ Assets/Scripts/MainMenu/PulsingLogoInMainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsingLogoInMainMenu : MonoBehaviour {
+
+    public RectTransform trans;
+    private float newScale;
+    private int freezeTime = 1;
+    private float sinus;
+
+
+    void Start()
+    {
+        newScale = 1.0f;
+        trans.localScale = new UnityEngine.Vector3(newScale, newScale, 1.0f);
+    }
+
+
+    void Update()
+    {
+
+        if (Time.timeSinceLevelLoad > freezeTime)
+        {
+            sinus = (Mathf.Sin((Time.timeSinceLevelLoad - freezeTime) * 4));
+            newScale = 1f + sinus / 64;
+            trans.localScale = new Vector3(newScale, newScale, 1.0f);
+        }
+    }
+}
