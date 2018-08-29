f2bcd3305afd79628ce2cdefa3984aa7de14f65c
Senk <paweldawidziak@gmail.com>
2018-08-29 12:00:10 +0200

    pulsing text on title screen done

---
+++ Assets/Scripts/TitleScreen/fadingText.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fadingText : MonoBehaviour {
+
+    public RectTransform trans;
+    public GameObject obb;
+
+    private float cosinus;
+
+    private int freezeTime = 1;
+
+    private float newScale;
+
+    private float newTransparency;
+
+    void Start()
+    {
+        newScale = 1.0f;
+        newTransparency = 0;
+        trans.localScale = new UnityEngine.Vector3(newScale, newScale, 1.0f);
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > freezeTime)
+        {
+            cosinus = -(Mathf.Cos((Time.timeSinceLevelLoad - freezeTime) * 2));
+
+            //SKALOWANIE
+            newScale = 1f + cosinus / 32;
+            trans.localScale = new Vector3(newScale, newScale, 1.0f);
+
+            //PRZEZROCZYSTOŚĆ
+            newTransparency = 1f + cosinus;
+            obb.GetComponent<CanvasGroup>().alpha = newTransparency;
+        }
+
+    }
+}
--- Assets/Scripts/TitleScreen/pulsingLogo.cs
+++ Assets/Scripts/TitleScreen/pulsingLogo.cs
@@ -5,26 +5,24 @@ using UnityEngine;
 public class pulsingLogo : MonoBehaviour {
 
     public RectTransform trans;
-    public float newScale;
-    public int freezeTime = 1;
-    float sinus;
+    private float newScale;
+    private int freezeTime = 1;
+    private float sinus;
 
-    // Use this for initialization
+
     void Start () {
         newScale = 1.0f;
         trans.localScale = new UnityEngine.Vector3(newScale, newScale, 1.0f);
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-        Debug.Log(Time.timeSinceLevelLoad);
+
         if (Time.timeSinceLevelLoad > freezeTime)
         {
-            sinus = (Mathf.Sin((Time.timeSinceLevelLoad - freezeTime) * 3));
+            sinus = (Mathf.Sin((Time.timeSinceLevelLoad - freezeTime) * 2));
             newScale = 1f + sinus/12;
-            trans.localScale = new UnityEngine.Vector3(newScale, newScale, 1.0f);
-
+            trans.localScale = new Vector3(newScale, newScale, 1.0f);
         }
-
     }
 }
