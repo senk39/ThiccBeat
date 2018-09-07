95f5c433be4459ebc9f07110421da654f7bbb666
Senk <paweldawidziak@gmail.com>
2018-09-07 22:25:05 +0200

    buttons in main menu are working now

---
+++ Assets/Scripts/MainMenu/sceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour {
+
+    public void clickPlayBtn()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void clickOptionsBtn()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void clickQuitBtn()
+    {
+        Application.Quit();
+    }
+
+}
+
