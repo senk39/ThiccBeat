3a95bc6ceb4dff18507f6dfe07e3d7a181fe4bb4
Senk <paweldawidziak@gmail.com>
2018-10-03 20:01:00 +0200

    Song list WIP

---
+++ Assets/SongIndex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongIndex : MonoBehaviour {
+
+    SongList listWithSongs;
+
+	// Use this for initialization
+	void Start () {
+        listWithSongs = GameObject.Find("Song List Itself").GetComponent<SongList>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/SongList.cs
+++ Assets/SongList.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class SongList : MonoBehaviour {
 
-    // Use this for initialization
-
-
-
     class Song
     {
         public static uint totalAmount = 0;
@@ -22,11 +18,14 @@ public class SongList : MonoBehaviour {
         public string genre2;
         public ushort BPM;
 
-        //time
-        //notes
+        public string audioLength;
+        public uint notes;
 
-        public GameObject cover;
-        public GameObject audioFile;
+        public byte difficultyEasy;
+        public byte difficultyHard;
+
+        public Sprite cover;
+        public AudioClip audioFile;
 
 
 
@@ -45,7 +44,10 @@ public class SongList : MonoBehaviour {
             totalAmount++;
         }
 
-        public Song(uint _index, string _title, string _producer, string _illustrator, string _lyricist, string _genre1, string _genre2, ushort _BPM)  // KONSTRUKTOR
+        public Song(uint _index, string _title, string _producer, string _illustrator, 
+                    string _lyricist, string _genre1, string _genre2, ushort _BPM, 
+                    string _audioLength, uint _notes, byte _difficultyEasy, byte _difficultyHard)  // KONSTRUKTOR
+
         {
             index = _index;
             title = _title;
@@ -56,7 +58,13 @@ public class SongList : MonoBehaviour {
             genre2 = _genre2;
             BPM = _BPM;
 
-            totalAmount++;
+            audioLength = _audioLength;
+            notes = _notes;
+
+            difficultyEasy = _difficultyEasy;
+            difficultyHard = _difficultyHard;
+
+        totalAmount++;
         }
 
         public void showInfo()
@@ -73,38 +81,21 @@ public class SongList : MonoBehaviour {
         }
     }
 
-    //Song Despacito = new Song();
-    
+    // START TWORZENIA PIOSENKÓW
+    Song piosenkaOPociagach = new Song(
+            0, "Piosenka o Pociagach", "Marvyanaka", "Marvyanaka", "Marvyanaka", "electropop", "Vocaloid", 150, "3:27", 429, 3, 7);
 
+    Song stardust = new Song(
+        1, "Stardust", "Senketsu", "NixieBlue", "yuyechka", "pop-rock", "Vocaloid", 150, "5:28", 594, 2, 6);
+
+    Song despacito = new Song(
+        2, "Despacito", "Louis Fonsi", "Louis Fonsi", null, "reggaeton", "latin pop", 120, "4:42", 666, 5, 10);
 
     void Start() {
 
-        Song piosenkaOPociagach = new Song(
-            0, "Piosenka o Pociagach", "Marvyanaka", "Marvyanaka", "Marvyanaka", "electropop", "Vocaloid", 150);
+        //MOŻE TWORZENIE PIOSENKÓW POWINNO PRZEBIEGAĆ W TYM MIEJSCU? MOŻE TRZEBA BĘDZIE PRZENIEŚĆ
 
-
-        Song stardust = new Song(
-            1, "Stardust", "Senketsu", "NixieBlue", "yuyechka", "pop-rock", "Vocaloid", 150);
-
-        Song despacito = new Song(
-            2,
-            "Despacito", "Louis Fonsi", "Andrzej Sapkowski", null, "pop", null, 120);
-
-
-        piosenkaOPociagach.showInfo();
-        stardust.showInfo();
+        despacito.showInfo();
    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
