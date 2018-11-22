2e5081ebd99fd74562f4db881cc1a760ef0842ac
Senk <paweldawidziak@gmail.com>
2018-11-22 12:29:59 +0100

    removed bug with overlapping midNote and added frame limitter to have rock solid audio sync

---
+++ Assets/frameLimiter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class frameLimiter : MonoBehaviour
+{
+    public int targetFrameRate = 60;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
