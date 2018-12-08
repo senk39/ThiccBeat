2127074cc0848c792cd811e3a63d34875e5e194f
Senk <paweldawidziak@gmail.com>
2018-12-08 23:06:46 +0100

    made the rest of notes, but they are too fast

--- Assets/noteGenerator.cs
+++ Assets/noteGenerator.cs
@@ -35,7 +35,15 @@ FULL BEAT: 12.09
     public TextAsset row4;
     public TextAsset row5;
     public TextAsset row6;
+
     public string textContent1;
+    public string textContent2;
+    public string textContent3;
+    public string textContent4;
+    public string textContent5;
+    public string textContent6;
+
+
     public string[] textContentSplit1;
 
 
@@ -52,7 +60,14 @@ FULL BEAT: 12.09
 
     int[] numbers;
     int tempValueRow1 = 0;
+    int tempValueRow2 = 0;
+    int tempValueRow3 = 0;
+    int tempValueRow4 = 0;
+    int tempValueRow5 = 0;
+    int tempValueRow6 = 0;
+
     const float link = 15.8808933002f;
+    const float oneMidiLength = 0.06296875f;
 
 
 
@@ -66,24 +81,78 @@ FULL BEAT: 12.09
        // InvokeRepeating("noteInstantiateForGenerator", 3f, distBetweenNotes);
 
         textContent1 = row1.text;
+        textContent2 = row2.text;
+        textContent3 = row3.text;
+        textContent4 = row4.text;
+        textContent5 = row5.text;
+        textContent6 = row6.text;
+
 
         string[] textContentSplit1 = textContent1.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+        string[] textContentSplit2 = textContent2.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+        string[] textContentSplit3 = textContent3.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+        string[] textContentSplit4 = textContent4.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+        string[] textContentSplit5 = textContent5.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+        string[] textContentSplit6 = textContent6.Split(new char[] { ' ', ',', '.', '\n', '\t' });
+
 
         foreach (string s in textContentSplit1)
         {
-
             if (s.Trim() != "")
             {
                 int.TryParse(s, out tempValueRow1);
                 Debug.Log(tempValueRow1);
-                Instantiate(gnote, new Vector3(-5.1f, 0.35f, (105.15f+(tempValueRow1/link))), gnoteq);
-
-
-                //tempValueRow1++;
+                Instantiate(gnote, new Vector3(-5.1f, 0.35f, (105.15f + (tempValueRow1 * oneMidiLength))), gnoteq);
             }
-                
         }
 
+        foreach (string s in textContentSplit2)
+        {
+            if (s.Trim() != "")
+            {
+                int.TryParse(s, out tempValueRow2);
+                Debug.Log(tempValueRow2);
+                //Instantiate(gnote, new Vector3(-3.1f, 0.35f, (105.15f + (tempValueRow2 / link))), gnoteq);
+                Instantiate(gnote, new Vector3(-3.1f, 0.35f, (105.15f + (tempValueRow2 * oneMidiLength))), gnoteq);
+            }
+        }
+
+        foreach (string s in textContentSplit3)
+        {
+            if (s.Trim() != "")
+            {
+                int.TryParse(s, out tempValueRow3);
+                Debug.Log(tempValueRow3);
+                Instantiate(gnote, new Vector3(-1.1f, 0.35f, (105.15f + (tempValueRow3 * oneMidiLength))), gnoteq);
+            }
+        }
+        foreach (string s in textContentSplit4)
+        {
+            if (s.Trim() != "")
+            {
+                int.TryParse(s, out tempValueRow4);
+                Debug.Log(tempValueRow4);
+                Instantiate(gnote, new Vector3(1.1f, 0.35f, (105.15f + (tempValueRow4 * oneMidiLength))), gnoteq);
+            }
+        }
+        foreach (string s in textContentSplit5)
+        {
+            if (s.Trim() != "")
+            {
+                int.TryParse(s, out tempValueRow5);
+                Debug.Log(tempValueRow5);
+                Instantiate(gnote, new Vector3(3.1f, 0.35f, (105.15f + (tempValueRow5 * oneMidiLength))), gnoteq);
+            }
+        }
+        foreach (string s in textContentSplit6)
+        {
+            if (s.Trim() != "")
+            {
+                int.TryParse(s, out tempValueRow6);
+                Debug.Log(tempValueRow6);
+                Instantiate(gnote, new Vector3(5.1f, 0.35f, (105.15f + (tempValueRow6 * oneMidiLength))), gnoteq);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -106,4 +175,7 @@ FULL BEAT: 12.09
     {
         //Instantiate(gnote, gnotevector, gnoteq);
     }
+
+
+
 }
