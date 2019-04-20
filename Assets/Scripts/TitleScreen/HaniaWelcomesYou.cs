46d14ea51bf9c8a7b260160c36474ef73b88c51f
Senk <paweldawidziak@gmail.com>
2019-04-20 19:25:24 +0200

    welcome screen done with Hania >w<

---
+++ Assets/Scripts/TitleScreen/HaniaWelcomesYou.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HaniaWelcomesYou : MonoBehaviour {
+
+    AudioClip[] HaniaWelcome = new AudioClip[13];
+    public List<AudioClip> HaniaList = new List<AudioClip>();
+    public GameObject[] HaniaObjArray = new GameObject[13];
+
+
+    // Use this for initialization
+    void Awake () {
+
+        HaniaObjArray = GameObject.FindGameObjectsWithTag("Hania");
+
+        for (int i = 0; i < HaniaObjArray.Length; i++)
+        {
+            HaniaList.Add(HaniaObjArray[i].gameObject.GetComponent<AudioSource>().clip);
+        }
+
+       // GetComponent<AudioSource>().clip = HaniaList[Random.Range(0,13)];
+        //GetComponent<AudioSource>().Play();
+        InvokeRepeating("sayAgain", 1f, 20f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        
+
+    }
+
+    void sayAgain()
+    {
+        GetComponent<AudioSource>().clip = HaniaList[Random.Range(0, 13)];
+
+        GetComponent<AudioSource>().Play();
+    }
+}
