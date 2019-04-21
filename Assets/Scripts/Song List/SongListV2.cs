a6cf67f18a79b9e879146c5223d9f7ee449d2e40
Senk <paweldawidziak@gmail.com>
2019-04-21 13:43:28 +0200

    diff tabs done

--- Assets/Scripts/Song List/SongListV2.cs
+++ Assets/Scripts/Song List/SongListV2.cs
@@ -49,6 +49,11 @@ public class SongListV2 : MonoBehaviour
     public Sprite hardBorder;
     public Sprite easyBorder;
 
+    public Sprite hardTab;
+    public Sprite easyTab;
+
+
+
 
     int goldScoreLabel;
     int silverScoreLabel;
@@ -398,11 +403,13 @@ public class SongListV2 : MonoBehaviour
         if(isCurrentDifficultyIsEasy == true)
         {
             GameObject.Find("Selected Song").GetComponent<Image>().sprite = easyBorder;
+            GameObject.Find("Difficulty Tab").GetComponent<Image>().sprite = easyTab;
 
         }
         else
         {
             GameObject.Find("Selected Song").GetComponent<Image>().sprite = hardBorder;
+            GameObject.Find("Difficulty Tab").GetComponent<Image>().sprite = hardTab;
         }
     }
 
