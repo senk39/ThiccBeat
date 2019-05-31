ff2f28d9f7972fdb98383f300f6d483c6c7b58c8
Senk <paweldawidziak@gmail.com>
2019-05-31 11:57:58 +0200

    deleted more songs and high scores

--- Assets/Scripts/Song List/SongListV2.cs
+++ Assets/Scripts/Song List/SongListV2.cs
@@ -29,33 +29,6 @@ public class SongListV2 : MonoBehaviour
     public GameObject selGenreObj;
     TextMeshProUGUI selGenreLabel;
 
-    public TextMeshProUGUI selHighScore1;
-    TextMeshProUGUI selHighScore1Label;
-
-    public TextMeshProUGUI selHighScore2;
-    TextMeshProUGUI selHighScore2Label;
-
-    public TextMeshProUGUI selHighScore3;
-    TextMeshProUGUI selHighScore3Label;
-
-    public TextMeshProUGUI selHighScore1Acc;
-    TextMeshProUGUI selHighScore1LabelAcc;
-
-    public TextMeshProUGUI selHighScore2Acc;
-    TextMeshProUGUI selHighScore2LabelAcc;
-
-    public TextMeshProUGUI selHighScore3Acc;
-    TextMeshProUGUI selHighScore3LabelAcc;
-
-    public TextMeshProUGUI selHighScore1Combo;
-    TextMeshProUGUI selHighScore1LabelCombo;
-
-    public TextMeshProUGUI selHighScore2Combo;
-    TextMeshProUGUI selHighScore2LabelCombo;
-
-    public TextMeshProUGUI selHighScore3Combo;
-    TextMeshProUGUI selHighScore3LabelCombo;
-
     public GameObject selCoverObj;
 
     public static int selectedSongByUser;
@@ -175,29 +148,6 @@ public class SongListV2 : MonoBehaviour
     static Song sixtrillion = new Song(
         1, "A Tale of Six Trillion Years and a Night", "kemu", "IA", null, null, "synth-rock", "vocaloid", 186, "1:42", 272, 522, 4, 10);
 
-    static Song hitorigoto = new Song(
-        2, "Hitorigoto", "ClariS", null, null, null, "j-pop", "anime", 165, "3:13", 203, 367, 2, 7);
-
-    static Song killthislove = new Song(
-        3, "Kill This Love", "BLACKPINK", null, null, null, "k-pop", null, 132, "3:13", 1, 485, 1, 5);
-
-    static Song badapple = new Song(
-        4, "Bad Apple!!", "Alstroemeria Records", "nomico", null, null, "edm", "touhou", 138, "3:13", 247, 451, 3, 6);
-
-    static Song japaripark = new Song(
-        5, "Youkoso Japari Park e", "Doubutsu Biscuits × PPP", null, null, null, "j-pop", "anime", 170, "3:13", 122, 344, 4, 8);
-
-    static Song dejavu = new Song(
-        6, "Deja Vu", "Dave Rodgers", null, null, null, "eurobeat", "anime", 154, "1:32", 171, 350, 4, 7);
-
-    static Song miraclewave = new Song(
-        7, "MIRACLE WAVE", "Aqours", null, null, null, "j-pop", "love live!", 183, "1:34", 283, 422, 5, 7);
-
-    static Song hibana = new Song(
-        8, "HIBANA", "DECO*27", "Hatsune Miku", null, null, "pop-rock", "vocaloid", 200, "1:15", 201, 389, 3, 8);
-
-
-
     void Awake()
     {
         selArtistLabel = selArtistObj.GetComponent<TMPro.TextMeshProUGUI>();
@@ -205,18 +155,6 @@ public class SongListV2 : MonoBehaviour
         selGenreLabel = selGenreObj.GetComponent<TMPro.TextMeshProUGUI>();
         selDiffLabel = selDiffObj.GetComponent<TMPro.TextMeshProUGUI>();
         selDiffAltLabel = selDiffAltObj.GetComponent<TMPro.TextMeshProUGUI>();
-
-        selHighScore1Label = selHighScore1.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore2Label = selHighScore2.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore3Label = selHighScore3.GetComponent<TMPro.TextMeshProUGUI>();
-
-        selHighScore1LabelAcc = selHighScore1Acc.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore2LabelAcc = selHighScore2Acc.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore3LabelAcc = selHighScore3Acc.GetComponent<TMPro.TextMeshProUGUI>();
-
-        selHighScore1LabelCombo = selHighScore1Combo.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore2LabelCombo = selHighScore2Combo.GetComponent<TMPro.TextMeshProUGUI>();
-        selHighScore3LabelCombo = selHighScore3Combo.GetComponent<TMPro.TextMeshProUGUI>();
 
         acChangeSong = acConChangeSong.GetComponent<AudioSource>();
         acChangeDiff = acConChangeDiff.GetComponent<AudioSource>();
@@ -248,13 +186,6 @@ public class SongListV2 : MonoBehaviour
         //allSongs.Add(comfyplace);
         allSongs.Add(aiaiai);
         allSongs.Add(sixtrillion);
-        allSongs.Add(hitorigoto);
-        allSongs.Add(killthislove);
-        allSongs.Add(badapple);
-        allSongs.Add(japaripark);
-        allSongs.Add(dejavu);
-        allSongs.Add(miraclewave);
-        allSongs.Add(hibana);
 
     }
 
@@ -336,7 +267,6 @@ public class SongListV2 : MonoBehaviour
         //DIFF FIELD
         starGenerator();
         //HIGH SCORE FIELD
-        highScores();
     }
 
     void movingOtherSongsUp()
@@ -428,154 +358,6 @@ public class SongListV2 : MonoBehaviour
         }
     }
 
-    void highScores()
-    {
-        if (isCurrentDifficultyIsEasy == true)
-        {
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldScoreeasy") > 1)
-            {
-                selHighScore1Label.text = "1. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldScoreeasy").ToString();
-                if(selHighScore1LabelAcc.text != null && selHighScore1LabelAcc.text != "xD" && selHighScore1LabelAcc.text != "0")
-                {
-                    selHighScore1LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "GoldAccuracyeasy");
-                    selHighScore1LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldComboeasy").ToString();
-
-                }
-                else
-                {
-                    selHighScore1LabelAcc.text = " ";
-                    selHighScore1LabelCombo.text = " ";
-                }
-
-            }
-            else
-            {
-                selHighScore1Label.text = "1. ---";
-                selHighScore1LabelAcc.text = " ";
-                selHighScore1LabelCombo.text = " ";
-            }
-
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverScoreeasy") > 1)
-            {
-                selHighScore2Label.text = "2. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverScoreeasy").ToString();
-                if (selHighScore2LabelAcc.text != null && selHighScore2LabelAcc.text != "xD" && selHighScore2LabelAcc.text != "0")
-                {
-                    selHighScore2LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "SilverAccuracyeasy");
-                    selHighScore2LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverComboeasy").ToString();
-
-                }
-                else
-                {
-                    selHighScore2LabelAcc.text = " ";
-                    selHighScore2LabelCombo.text = " ";
-                }
-            }
-            else
-            {
-                selHighScore2Label.text = "2. ---";
-                selHighScore2LabelAcc.text = " ";
-                selHighScore2LabelCombo.text = " ";
-
-            }
-
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeScoreeasy") > 1)
-            {
-                selHighScore3Label.text = "3. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeScoreeasy").ToString();
-                if (selHighScore3LabelAcc.text != null && selHighScore3LabelAcc.text != "xD" && selHighScore3LabelAcc.text != "0")
-                {
-                    selHighScore3LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "BronzeAccuracyeasy");
-                    selHighScore3LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeComboeasy").ToString();
-
-                }
-                else
-                {
-                    selHighScore3LabelAcc.text = " ";
-                    selHighScore3LabelCombo.text = " ";
-                }
-            }
-            else
-            {
-                selHighScore3Label.text = "3. ---";
-                selHighScore3LabelAcc.text = " ";
-                selHighScore3LabelCombo.text = " ";
-            }
-
-        }
-        else
-        {
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldScorehard") > 1)
-            {
-                selHighScore1Label.text = "1. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldScorehard").ToString();
-                if (selHighScore1LabelAcc.text != null && selHighScore1LabelAcc.text != "xD" && selHighScore1LabelAcc.text != "0")
-                {
-                    selHighScore1LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "GoldAccuracyhard");
-                    selHighScore1LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "GoldCombohard").ToString();
-
-                }
-                else
-                {
-                    //selHighScore1Label.text = "1. ---";
-                    selHighScore1LabelAcc.text = " ";
-                    selHighScore1LabelCombo.text = " ";
-                }
-            }
-            else
-            {
-                selHighScore1Label.text = "1. ---";
-                selHighScore1LabelAcc.text = " ";
-                selHighScore1LabelCombo.text = " ";
-            }
-
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverScorehard") > 1)
-            {
-                selHighScore2Label.text = "2. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverScorehard").ToString();
-                if (selHighScore2LabelAcc.text != null && selHighScore2LabelAcc.text != "xD" && selHighScore2LabelAcc.text != "0")
-                {
-                    selHighScore2LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "SilverAccuracyhard");
-                    selHighScore2LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "SilverCombohard").ToString();
-
-                }
-                else
-                {
-                    //selHighScore2Label.text = "2. ---";
-                    selHighScore2LabelAcc.text = " ";
-                    selHighScore2LabelCombo.text = " ";
-                }
-            }
-            else
-            {
-                selHighScore2Label.text = "2. ---";
-                selHighScore2LabelAcc.text = " ";
-                selHighScore2LabelCombo.text = " ";
-            }
-
-            if (PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeScorehard") > 1)
-            {
-                selHighScore3Label.text = "3. " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeScorehard").ToString();
-                if (selHighScore3LabelAcc.text != null && selHighScore3LabelAcc.text != "xD" && selHighScore3LabelAcc.text != "0")
-                {
-                    selHighScore3LabelAcc.text = PlayerPrefs.GetString(allSongs[selectedSongByUser].index + "BronzeAccuracyhard");
-                    selHighScore3LabelCombo.text = "combo: " + PlayerPrefs.GetInt(allSongs[selectedSongByUser].index + "BronzeCombohard").ToString();
-
-                }
-                else
-                {
-                    //selHighScore3Label.text = "3. ---";
-                    selHighScore3LabelAcc.text = " ";
-                    selHighScore3LabelCombo.text = " ";
-                }
-            }
-            else
-            {
-                selHighScore3Label.text = "3. ---";
-                selHighScore3LabelAcc.text = " ";
-                selHighScore3LabelCombo.text = " ";
-            }
-        }
-
-
-    }
-
     void alternativeDifficultyTextGenerator()
     {
         if (isCurrentDifficultyIsEasy)
@@ -598,7 +380,6 @@ public class SongListV2 : MonoBehaviour
                 acChangeDiff.Play();
                 isCurrentDifficultyIsEasy = true;
                 starGenerator();
-                highScores();
             }
 
             else
@@ -606,7 +387,6 @@ public class SongListV2 : MonoBehaviour
                 acChangeDiff.Play();
                 isCurrentDifficultyIsEasy = false;
                 starGenerator();
-                highScores();
             }
         }
     }
